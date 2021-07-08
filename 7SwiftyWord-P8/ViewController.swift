e32f3d57e3cb1b1e5d306d8b89c1177ca8f654a7
Łukasz Nycz <lnycz96@gmail.com>
2021-07-08 12:41:55 +0200

    Initial Commit

---
+++ 7SwiftyWord-P8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7SwiftyWord-P8
+//
+//  Created by Łukasz Nycz on 08/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
